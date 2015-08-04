66777b0d77d58bed65b744241c303f4aa060cad7
Hadia Hameed <hadiahameeduet@gmail.com>
2015-08-04 10:11:10 +0500

    First Commit: Scaffold created. gitignore updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '836d43ca5290a12234e22135a204af8fe0de024acc790b5a6f6b00b6f3e5bab28704073e568fb8ac1ae4736c06ec46f598341743f10b05f7a2b833c389c44088'
