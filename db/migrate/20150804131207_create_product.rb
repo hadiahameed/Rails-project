2ae93c27ec8fc548c567628b196a44d7cba549d7
Hadia Hameed <hadiahameeduet@gmail.com>
2015-08-05 10:55:49 +0500

    Migrations Issue Resolved.

---
+++ db/migrate/20150804131207_create_product.rb
@@ -0,0 +1,10 @@
+class CreateProduct < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
