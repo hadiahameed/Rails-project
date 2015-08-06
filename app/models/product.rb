07f2238a363b47d6754f6609a0a7a73fb8172df3
Hadia Hameed <hadiahameeduet@gmail.com>
2015-08-06 15:50:12 +0500

    Product: Attachments destroyable.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 
   PER_PAGE_NUMBER = 20
   attr_accessible :description, :name, :attachments_attributes
-  has_many :attachments, as: :attachable
+  has_many :attachments, as: :attachable, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :attachments, allow_destroy: true
   belongs_to :user
