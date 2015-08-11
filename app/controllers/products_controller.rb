50077842f17a2b9f1f2e0a15bc69202752c81088
Hadia Hameed <hadiahameeduet@gmail.com>
2015-08-11 12:36:56 +0500

    Products ordered by created_at date

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.page(params[:page]).per(Product::PER_PAGE_NUMBER)
+    @products = Product.order('created_at DESC').page(params[:page]).per(Product::PER_PAGE_NUMBER)
 
 
     respond_to do |format|
